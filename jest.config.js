b9cf7ca9f3e00e8a967c0cac08361e51c6307dc3
lucashrv <lucas.hrv@outlook.com>
2025-08-01 08:49:03 -0300

    chore: Add Jest configs

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import("jest").Config} **/
+export default {
+  roots: ["<rootDir>/src"],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.ts"],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
