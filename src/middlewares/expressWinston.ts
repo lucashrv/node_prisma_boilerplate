2eb84912eb52c5e11c7fd15a3c52c614d97af64c
lucashrv <lucas.hrv@outlook.com>
2025-08-06 16:09:43 -0300

    fix(expressWinston): middleware refactoring

--- src/middlewares/expressWinston.ts
+++ src/middlewares/expressWinston.ts
@@ -5,28 +5,21 @@ const { combine, printf, colorize, timestamp } = format;
 
 const isProd = process.env.NODE_ENV === "production";
 
-const consoleFormat = printf(({ level, message, timestamp }) => {
-    return `[${timestamp}] ${level}: ${message}`;
-});
-
-const fileFormatLogger = printf(
+const loggerFormat = printf(
     ({ level, message, timestamp }) => `[${timestamp}] ${level}: ${message}`,
 );
 
 export const expressWinstonLogger = expressWinston.logger({
-    level:
-        process.env.NODE_ENV === "production"
-            ? "info"
-            : process.env.LOG_LEVEL || "debug",
+    level: isProd ? "info" : process.env.LOG_LEVEL || "debug",
     transports: [
         new transports.Console({
             level: isProd ? "error" : "debug",
-            format: combine(colorize(), consoleFormat),
+            format: combine(colorize(), loggerFormat),
         }),
         new transports.File({ filename: "logs/combined.log" }),
         new transports.File({ filename: "logs/error.log", level: "error" }),
     ],
-    format: combine(timestamp(), fileFormatLogger),
+    format: combine(timestamp(), loggerFormat),
     meta: true,
     msg: (req, res) => `HTTP ${req.method} ${req.url} ${res.statusCode}`,
     expressFormat: false,
