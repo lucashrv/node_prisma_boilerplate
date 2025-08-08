9d75f74d1542fb634212b6efddfe378feb6b1695
lucashrv <lucas.hrv@outlook.com>
2025-08-08 10:42:33 -0300

    feat(redis): add redis config and cache

--- src/services/usersServices.ts
+++ src/services/usersServices.ts
@@ -1,9 +1,16 @@
 import { User } from "@prisma/client";
 import { handleFactory } from "./handlers/handleFactory";
+import { redis } from "@/utils/redis";
 
 class UsersServices {
     public getAllUsers = async (): Promise<User[]> => {
+        const usersCache = await redis.get("users-get-all");
+        if (usersCache) return JSON.parse(usersCache);
+
         const users = await handleFactory.getAll("user");
+
+        await redis.set("users-get-all", JSON.stringify(users), "EX", 60 * 10);
+
         return users;
     };
 
